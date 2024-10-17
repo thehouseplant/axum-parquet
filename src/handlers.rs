f7cbcf73f65b9ec6c58b06f5000d576766e61b4d
Sean Collins <sean.collins@outlook.com>
2024-10-16 17:08:35 -0700

    Add handlers for add and retrieve functions

---
+++ src/handlers.rs
@@ -0,0 +1,28 @@
+use axum::{extract::Extension, http::StatusCode, Json};
+use serde_json::json;
+use std::sync::Arc;
+
+use crate::db::DbError;
+use crate::{db::Database, models::Record};
+
+pub async fn get_records_handler(
+    Extension(db): Extension<Arc<Database>>,
+) -> Result<Json<Vec<Record>>, (StatusCode, String)> {
+    match db.get_records() {
+        Ok(records) => Ok(Json(records)),
+        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_Sstring())),
+    }
+}
+
+pub async fn add_record_handler(
+    Extension(db): Extension<Arc<Database>>,
+    Json(payload): Json<Record>,
+) -> Result<(StatucCode, Json<serde_json::Value>), (StatusCode, String)> {
+    match db.add_record(payload.clone()) {
+        Ok(_) => Ok((
+            StatusCode::CREATED,
+            Json(json!({"status": "success", "record": payload})),
+        )),
+        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
+    }
+}
